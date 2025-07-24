602c7b9954391f933da90af83b1606e88ca304c0
Sibu088 <mavismadira6@gmail.com>
2025-07-24 08:07:45 +0200

    Chapter1.sql

---
+++ chapter1.sql try it yourself.sql
@@ -0,0 +1,8 @@
+INSERT INTO animals (name, birth_date, gender, arrival_date, animal_type_id)
+VALUES 
+('Simba', '2020-05-12', 'Male', '2021-03-01', 1),
+('Nala', '2021-08-22', 'Female', '2022-01-10', 1),
+('Dumbo', '2019-03-15', 'Male', '2020-07-04', 2),
+('Pabu', '2022-02-11', 'Male', '2023-03-09', 3);
+
+
